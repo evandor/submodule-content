640c8b8f749f7b81db57ec3edf1ef6c0c4def653
Carsten Gräf <evandor@gmail.com>
2025-04-29 07:16:58 +0200

    getting ready for release

--- stores/contentStore.ts
+++ stores/contentStore.ts
@@ -44,7 +44,7 @@ export const useContentStore = defineStore('content', () => {
   watchEffect(() => {
     currentTabReferences.value = []
     if (currentTabContent.value.trim().length > 0) {
-      //console.debug('updating content store...')
+      console.debug('updating content store...')
       const $ = cheerio.load(currentTabContent.value)
       checkLinks($)
       checkMeta($)
